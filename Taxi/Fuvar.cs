0f76ef2357ad0f2c864a6414ef10fe737c167042
tothtamas9 <ttamas026@gmail.com>
2023-10-09 18:56:10 +0200

    Add project files.

---
+++ Taxi/Fuvar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Fuvar
+    {
+        int azonosito;
+        string indulas_idopont;
+        int utazas_idotartama;
+        double tavolsag;
+        double viteldij;
+        double borravalo;
+        string fizetes_modja;
+
+        public Fuvar(int azonosito, string indulas_idopont, int utazas_idotartama, double tavolsag, double viteldij, double borravalo, string fizetes_modja)
+        {
+            this.azonosito = azonosito;
+            this.indulas_idopont = indulas_idopont;
+            this.utazas_idotartama = utazas_idotartama;
+            this.tavolsag = tavolsag;
+            this.viteldij = viteldij;
+            this.borravalo = borravalo;
+            this.fizetes_modja = fizetes_modja;
+        }
+
+        public int Azonosito { get => azonosito; }
+
+        //2016-12-15 23:45:00
+        public int Indulas_ev { get => int.Parse(indulas_idopont.Substring(0, 4)); }
+        public int Indulas_honap { get => int.Parse(indulas_idopont.Substring(5, 2)); }
+        public int Indulas_nap { get => int.Parse(indulas_idopont.Substring(8, 2)); }
+        public int Indulas_ora { get => int.Parse(indulas_idopont.Substring(11, 2)); }
+        public int Indulas_perc { get => int.Parse(indulas_idopont.Substring(14, 2)); }
+        public int Indulas_mperc { get => int.Parse(indulas_idopont.Substring(17, 2)); }
+        public int Utazas_idotartama { get => utazas_idotartama; }
+        public double Tavolsag { get => tavolsag; }
+        public double Viteldij { get => viteldij; }
+        public double Borravalo { get => borravalo; }
+        public string Fizetes_modja { get => fizetes_modja; }
+
+        
+    }
+}
---
+++ Taxi/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fuvar> fuvarok = new List<Fuvar>();
+            string[] olvasottSorok = File.ReadAllLines("data\\fuvar.csv");
+
+            for (int i = 1; i < olvasottSorok.Length; i++)
+            {
+                string[] tagok = olvasottSorok[i].Split(';');
+                Fuvar ujFuvar = new Fuvar(int.Parse(tagok[0]), tagok[1], int.Parse(tagok[2]), double.Parse(tagok[3]), double.Parse(tagok[4]), double.Parse(tagok[5]), tagok[6]);
+                fuvarok.Add(ujFuvar);
+            }
+            //3. Feladat:
+            Console.WriteLine($"3. feladat: {fuvarok.Count()} fuvar");
+
+            //4. Feladat:
+            int fuvarok_szama = 0;
+            double ossz_bevetel = 0;
+
+            foreach (var fuvar in fuvarok)
+            {
+                if (fuvar.Azonosito == 6185)
+                {
+                    fuvarok_szama++;
+                    ossz_bevetel += fuvar.Viteldij + fuvar.Borravalo;
+                }
+            }
+            Console.WriteLine($"4. feladat: {fuvarok_szama} fuvar alatt: {ossz_bevetel}$");
+
+            //5. Feladat:
+            int bankkartyas = 0;
+            int keszpenzes = 0;
+            int vitatott = 0;
+            int ingyenes = 0;
+            int ismeretlen = 0;
+            Console.WriteLine("5.feladat:");
+            foreach (var fuvar in fuvarok)
+            {
+                if (fuvar.Fizetes_modja == "bankkártya")
+                {
+                    bankkartyas++;
+                }
+                else if (fuvar.Fizetes_modja == "készpénz")
+                {
+                    keszpenzes++;
+                }
+                else if (fuvar.Fizetes_modja == "vitatott")
+                {
+                    vitatott++;
+                }
+                else if (fuvar.Fizetes_modja == "ingyenes")
+                {
+                    ingyenes++;
+                }
+                else
+                {
+                    ismeretlen++;
+                }
+            }
+
+            Console.WriteLine($"\t bankkártya: {bankkartyas} fuvar");
+            Console.WriteLine($"\t készpénz: {keszpenzes} fuvar");
+            Console.WriteLine($"\t vitatott: {vitatott} fuvar");
+            Console.WriteLine($"\t ingyenes: {ingyenes} fuvar");
+            Console.WriteLine($"\t ismeretlen: {ismeretlen} fuvar");
+
+        }
+    }
+}
